c129d0d2b5d4f095dd29a83fb5b076d9a6c8c796
junseooo <pointjunseo@gmail.com>
2024-09-20 17:03:39 +0900

    formattedDate 함수 초 단위까지 보이도록 수정

--- front-end/src/lib/formattedDate.ts
+++ front-end/src/lib/formattedDate.ts
@@ -2,5 +2,5 @@ import moment from "moment";
 import "moment/locale/ko";
 
 export default function formattedDate(date: Date): string {
-  return moment(date).locale("ko").format("YYYY년 M월 D일 dddd HH:mm");
+  return moment(date).locale("ko").format("YYYY년 M월 D일 dddd HH:mm:ss");
 }
