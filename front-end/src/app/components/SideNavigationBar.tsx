bc44b7c2cd9fb9789d01ad57013605530eba733d
junseooo <pointjunseo@gmail.com>
2024-09-23 16:42:49 +0900

    사용하지 않는 코드 삭제

--- front-end/src/app/components/SideNavigationBar.tsx
+++ front-end/src/app/components/SideNavigationBar.tsx
@@ -30,8 +30,6 @@ export default async function SideNavigationBar() {
           label: category.label,
         })),
     },
-    // { category: { href: "/about", label: "소개" } },
-    // { category: { href: "/contact", label: "연락처" } },
   ];
 
   return (
--- front-end/src/lib/posts.ts
+++ front-end/src/lib/posts.ts
@@ -1,6 +1,5 @@
 import { IPost } from "@/app/interfaces";
 import fs from "fs/promises";
-import matter from "gray-matter";
 import path from "path";
 
 const postsDirectory = path.join(process.cwd(), "data/posts/");
@@ -27,50 +26,6 @@ export async function getRecentPosts(count: number): Promise<IPost[] | null> {
   }
 }
 
-// 로컬에 저장하는 파일이 .mdx 파일인 경우
-// export async function getAllPosts(): Promise<IPost[]> {
-//   if (cachedPosts.length > 0) {
-//     return cachedPosts;
-//   }
-
-//   try {
-//     const fileNames = await fs.readdir(postsDirectory);
-//     const allPostsData = await Promise.all(
-//       fileNames.map(async (fileName) => {
-//         const slug = fileName.replace(".mdx", "");
-//         const fullPath = path.join(postsDirectory, fileName);
-//         const fileContents = await fs.readFile(fullPath, "utf8");
-//         const { data, content } = matter(fileContents);
-
-//         return {
-//           id: data.id || Date.now(),
-//           slug,
-//           title: data.title || "Untitled",
-//           category: {
-//             href: data.category?.href || "",
-//             label: data.category?.label || "",
-//           },
-//           authorId: data.authorId || "",
-//           createdAt: new Date(data.createdAt || new Date()),
-//           updatedAt: new Date(data.updatedAt || new Date()),
-//           content,
-//           readingTime: Math.ceil(content.split(" ").length / 200),
-//         } as IPost;
-//       })
-//     );
-
-//     cachedPosts = allPostsData.sort(
-//       (a, b) => b.updatedAt.getTime() - a.updatedAt.getTime()
-//     );
-
-//     return cachedPosts;
-//   } catch (error) {
-//     console.error("Error fetching all posts:", error);
-//     return [];
-//   }
-// }
-
-// 로컬에 저장한 파일이 .json 파일인 경우
 export async function getAllPosts(): Promise<IPost[]> {
   if (cachedPosts.length > 0) {
     return cachedPosts;
