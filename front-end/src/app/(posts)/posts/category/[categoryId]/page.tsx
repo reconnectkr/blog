bb0f390f2ce1be66af79944bb212760b8e9e4941
junseooo <pointjunseo@gmail.com>
2024-10-02 14:46:20 +0900

    dependency array 추가

--- front-end/src/app/(posts)/posts/category/[categoryId]/page.tsx
+++ front-end/src/app/(posts)/posts/category/[categoryId]/page.tsx
@@ -23,7 +23,7 @@ export default function CategoryPage({
     };
 
     fetchPostsByCategory();
-  }, []);
+  }, [params.categoryId]);
 
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
