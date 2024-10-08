0424a564321e3c08f5c273b76243dee38a74e719
junseooo <pointjunseo@gmail.com>
2024-10-08 15:50:36 +0900

    Header에 회원가입 추가

--- front-end/src/app/components/Header.tsx
+++ front-end/src/app/components/Header.tsx
@@ -9,7 +9,7 @@ import Dialog from "./Dialog";
 export default function Header() {
   const [dialogState, setDialogState] = useState<{
     isOpen: boolean;
-    type: "login" | "logout" | null;
+    type: "login" | "logout" | "signup" | null;
   }>({ isOpen: false, type: null });
 
   const router = useRouter();
@@ -33,6 +33,15 @@ export default function Header() {
     logout();
   };
 
+  const handleSignup = () => {
+    setDialogState({ isOpen: true, type: "signup" });
+  };
+
+  const handleSignupConfirm = () => {
+    setDialogState({ isOpen: false, type: null });
+    router.push("/signup");
+  };
+
   const handleDialogClose = () => {
     setDialogState({ isOpen: false, type: null });
   };
@@ -51,7 +60,10 @@ export default function Header() {
             <button onClick={handleLogout}>로그아웃</button>
           </div>
         ) : (
-          <button onClick={handleLogin}>로그인</button>
+          <div className="flex flex-row gap-10">
+            <button onClick={handleSignup}>회원가입</button>
+            <button onClick={handleLogin}>로그인</button>
+          </div>
         )}
       </header>
       <Dialog
@@ -59,15 +71,25 @@ export default function Header() {
         onClick={
           dialogState.type === "login"
             ? handleLoginConfirm
-            : handleLogoutConfirm
+            : dialogState.type === "logout"
+            ? handleLogoutConfirm
+            : handleSignupConfirm
         }
         onClose={handleDialogClose}
-        title={dialogState.type === "login" ? "로그인" : "로그아웃"}
+        title={
+          dialogState.type === "login"
+            ? "로그인"
+            : dialogState.type === "logout"
+            ? "로그아웃"
+            : "회원가입"
+        }
       >
         <p>
           {dialogState.type === "login"
             ? "로그인 하시겠습니까?"
-            : "로그아웃 하시겠습니까?"}
+            : dialogState.type === "logout"
+            ? "로그아웃 하시겠습니까?"
+            : "회원가입을 하시겠습니까?"}
         </p>
       </Dialog>
     </div>
